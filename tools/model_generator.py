5cbc912a211df7ae2e11c76032a86d25b3fe285c
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-06-27 10:15:03 +0100

    add error handling to ._name_model()

--- tools/model_generator.py
+++ tools/model_generator.py
@@ -16,11 +16,47 @@ class ModelGenerator:
         self.target = target
         self.models = {}
 
+    def _name_model(
+        self,
+        model_function: Callable[..., Any],
+        params: dict = None,
+        index: int = None,
+        prefix: str = None,
+    ):
+        """
+        Generates model name from order.
+        If not provided or missing, uses the index.
+
+        ::parameters::
+        - model_function: The function or class to create the model (e.g., ARIMA, LinearRegression).
+        - prefix: str. Optional. If provided, used instead of model_function.
+        - params: dict. Optional. Should contain order argument as tuple for ARIMA models.
+        - index: int. Optional. Index to use if no order is available.
+        """
+        order = params.get("order")
+
+        model_name = str(prefix) if prefix else model_function.__name__
+
+        if (order and index) or (order is None and index is None):
+            raise ValueError(
+                "Cannot generate model name: Provide one of either a valid 'params' with 'order', or 'index'."
+            )
+
+        if order and isinstance(order, tuple) and len(order) == 3:
+            return f"{model_name}_{order[0]}_{order[1]}_{order[2]}"
+
+        if index and isinstance(index, int):
+            return f"{model_name}_{index}"
+
+        raise ValueError(
+            "Cannot generate model name: Invalid arguments."
+        )
+
     def generate_models(
         self,
         model_function: Callable[..., Any],
-        model_name_prefix: str,
-        param_grid: list[dict]
+        param_grid: list[dict],
+        model_name_prefix: str = None,
     ) -> None:
         """
         Generate models using the provided model function and parameter grid.
@@ -32,14 +68,15 @@ class ModelGenerator:
         """
 
         for i, params in enumerate(param_grid):
-            model_name = f"{model_name_prefix}_{i + 1}"
+            # Generate model name
+            model_name: str = self._name_model(model_function=model_function, params=params)
             model = model_function(self.data, **params)
 
             if self.target is not None:
                 result = model.fit(self.target)
             else:
                 result = model.fit()
-            
+
             self.models[model_name] = (model, result)
 
     def get_model(self, name: str) -> tuple[Any, Any] | None:
