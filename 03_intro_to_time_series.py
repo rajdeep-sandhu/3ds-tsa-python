f22dc9d6fee56c85a36c4aa99b38ac40623ddc50
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-09-01 09:48:47 +0100

    create marimo notebook 03_intro_to_time_series.py

---
+++ 03_intro_to_time_series.py
@@ -0,0 +1,224 @@
+import marimo
+
+__generated_with = "0.15.2"
+app = marimo.App()
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Import the Packages
+
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    return pd, plt, sns
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Importing the Data
+
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        Get closing prices of market indices:
+
+        - spx: S&P500
+        - dax: DAX 30
+        - ftse: FTSE 100
+        - nikkei: NIKKEI 225
+
+        These measure the stability of the following stock exchanges: US, Germany, London, Japan
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(pd):
+    raw_csv_data = pd.read_csv("Index2018.csv")
+    return (raw_csv_data,)
+
+
+@app.cell
+def _(raw_csv_data):
+    df_comp = raw_csv_data.copy()
+    return (df_comp,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Examining the Data
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(df_comp):
+    df_comp.head()
+    return
+
+
+@app.cell
+def _(df_comp):
+    # Get an overview of the numeric fields
+    df_comp.describe()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        - The mean of spx is much lower than for other indices.
+        - Its maximum is lower than the minimums of ftse and nikkei.
+        - Tha values for dax and ftse are similar, while those of spx are far smaller and, nikkey, much larger.
+        - This difference in magnitude needs to be taken into account when comparing multiple time series.
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(df_comp):
+    df_comp.info()
+    return
+
+
+@app.cell
+def _(df_comp):
+    df_comp["spx"].isna().sum()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### Plotting the Data
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(sns):
+    sns.set_theme(context="notebook", style="white")
+    return
+
+
+@app.cell
+def _(df_comp, plt):
+    df_comp["spx"].plot(figsize=(20, 5), title="S&P00")
+    plt.show()
+    return
+
+
+@app.cell
+def _(df_comp, plt):
+    df_comp["ftse"].plot(figsize=(20, 5), title="S&P00")
+    plt.show()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        - The first two peaks are the dotcom and the housing market bubbles respectively. These are mirrored by S&P00 and FTSE. This can be explained by the parallels between the US and the UK stock exchange markets.
+
+        """
+    )
+    return
+
+
+@app.cell
+def _(df_comp, plt):
+    plt.figure(figsize=(20, 5))
+    df_comp["spx"].plot(label="S&P500")
+    df_comp["ftse"].plot(label="FTSE100")
+    plt.title("S&P00 vs FTSE100")
+    plt.legend()
+    plt.show()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        - When plotted together, the S&P curve flattens. This does not imply relative stability and is due to the difference in magnitude of values between the two indices. This is one of the reasons why time series analysis involves compounded returns along with prices.
+
+        """
+    )
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        ### The QQ Plot
+
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    import scipy.stats
+    import pylab
+    return pylab, scipy
+
+
+@app.cell
+def _(df_comp, pylab, scipy):
+    scipy.stats.probplot(df_comp['spx'], plot=pylab)
+    pylab.show()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(
+        r"""
+        The dataset is not normally distributed, which means that the statistics of normal distributions cannot be used to make successful forecasts. However, this is as expected for time series data.
+        """
+    )
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+if __name__ == "__main__":
+    app.run()
