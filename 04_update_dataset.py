d6701becd57c375b2b748fd8cf489a03d506a8f8
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-09-03 10:15:13 +0100

    wrap yfinance.download() in a chached function to prevent repeated API requests on cell reruns

--- 04_update_dataset.py
+++ 04_update_dataset.py
@@ -31,27 +31,21 @@ def _(warnings):
 
 
 @app.cell
-def _(yfinance):
-    # Using the .download() method to get our data
+def _(mo, yfinance):
+    # Download the data
+    @mo.cache
+    def download_data():
+        return yfinance.download(
+            tickers="^GSPC ^FTSE ^N225 ^GDAXI",
+            start="1994-01-07",
+            end="2019-09-27",
+            interval="1d",
+            group_by="ticker",
+            auto_adjust=True,
+            threads=True,
+        )
 
-    raw_data = yfinance.download (
-        tickers = "^GSPC ^FTSE ^N225 ^GDAXI",
-        start = "1994-01-07",
-        end = "2019-09-27",
-        interval = "1d",
-        group_by = 'ticker',
-        auto_adjust = True,
-        threads = True)
-
-    # tickers -> The time series we are interested in - (in our case, these are the S&P, FTSE, NIKKEI and DAX)
-    # start -> The starting date of our data set
-    # end -> The ending date of our data set (at the time of upload, this is the current date)
-    # interval -> The distance in time between two recorded observations. Since we're using daily closing prices, we set it equal to "1d", which indicates 1 day. 
-    # group_by -> The way we want to group the scraped data. Usually we want it to be "ticker", so that we have all the information about a time series in 1 variable.
-    # auto_adjust -> Automatically adjust the closing prices for each period. 
-    # treads - > Whether to use threads for mass downloading.
-
-    raw_data
+    raw_data = download_data()
     return (raw_data,)
 
 
@@ -59,6 +53,7 @@ def _(yfinance):
 def _(raw_data):
     # Creating a back up copy in case we remove/alter elements of the data by mistake
     df_comp = raw_data.copy()
+    df_comp
     return (df_comp,)
 
 
